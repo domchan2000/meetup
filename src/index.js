808dda938307c13e418cc4e6a944d0249efb75a7
Ka Hei Chan <domchan2000@gmail.com>
2023-05-18 14:54:32 -0400

    edit BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 import { FavoritesContextProvider } from "./store/favourites-context";
@@ -8,8 +8,8 @@ import { FavoritesContextProvider } from "./store/favourites-context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <FavoritesContextProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </FavoritesContextProvider>
 );
