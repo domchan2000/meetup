098c82da84eacff4174f1fbbd1179f1e8f6d3770
Ka Hei Chan <domchan2000@gmail.com>
2023-05-18 19:15:01 -0400

    add google sign-in draft

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import AllMeetupsPage from "./pages/AllMeetup";
 import FavoritesPage from "./pages/Favourites";
 import NewMeetupPage from "./pages/NewMeetup";
 import Layout from "./components/layout/Layout";
+import { GoogleLogin } from '@react-oauth/google';
+
 
 function App() {
+  const responseMessage = (response) => {
+    console.log(response);
+};
+const errorMessage = (error) => {
+    console.log(error);
+};
+
   return (
       <Layout>
+      <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import App from "./App";
 import { FavoritesContextProvider } from "./store/favourites-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <GoogleOAuthProvider clientId="338831568014-ke7l996l0tqb1gavvh61k1oa9heqnana.apps.googleusercontent.com">
   <FavoritesContextProvider>
     <HashRouter>
       <App />
     </HashRouter>
   </FavoritesContextProvider>
+  </GoogleOAuthProvider>
+
 );
